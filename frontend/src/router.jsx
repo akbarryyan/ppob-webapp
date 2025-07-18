58557be4c70252e7b69ad0d1bf124c7449b35e29
akbarryyan <akbarrayyan182@gmail.com>
2025-07-19 01:53:29 +0700

    feat: Remove Payment and Profile pages from routing

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,8 +3,6 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Payment from "./pages/Payment";
-import Profile from "./pages/Profile";
 import NewTransaction from "./pages/NewTransaction";
 
 const router = createBrowserRouter([
@@ -31,14 +29,6 @@ const router = createBrowserRouter([
   {
     path: "/new-transaction",
     element: <NewTransaction />,
-  },
-  {
-    path: "/payment",
-    element: <Payment />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
   },
 ]);
 
